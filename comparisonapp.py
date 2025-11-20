85c710bcade4f91243eefe633ca0abf8ba9b73f7
SoxSavant <sambro449@gmail.com>
2025-11-20 00:42:57 -0500

    AG Grid fix

--- comparisonapp.py
+++ comparisonapp.py
@@ -1703,7 +1703,8 @@ with stat_builder_container:
         """
     )
 
-    gb = GridOptionsBuilder.from_dataframe(stat_config_df)
+    gb = GridOptionsBuilder()
+    gb.configure_columns(list(stat_config_df.columns))
     gb.configure_default_column(
         editable=True,
         filter=False,
--- teamapp.py
+++ teamapp.py
@@ -828,7 +828,8 @@ with stat_builder_container:
     )
     stat_config_df.insert(0, "Drag", ["↕"] * len(stat_config_df))
 
-    gb = GridOptionsBuilder.from_dataframe(stat_config_df)
+    gb = GridOptionsBuilder()
+    gb.configure_columns(list(stat_config_df.columns))
     gb.configure_default_column(
         editable=True,
         filter=False,
