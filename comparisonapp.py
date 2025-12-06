58abb1deee84a1e0c7d66ab46b7cfe328c2b8a91
SoxSavant <sambro449@gmail.com>
2025-12-05 22:39:46 -0500

    empty set

--- comparisonapp.py
+++ comparisonapp.py
@@ -246,12 +246,7 @@ STAT_PRESETS = {
         "LD%",
     ],
     "Blank – Create your own": [
-        "Off", "Def", "BsR", "WAR", "Barrel%", "HardHit%", "EV", "MaxEV",
-    "wRC+", "wOBA", "xwOBA", "xBA", "xSLG", "OPS", "SLG", "OBP", "AVG", "ISO",
-    "BABIP", "G", "PA", "AB", "R", "RBI", "HR", "XBH", "H", "1B", "2B", "3B", "SB", "BB", "IBB", "SO",
-    "K%", "BB%", "K-BB%", "O-Swing%", "Z-Swing%", "Swing%", "Contact%", "WPA", "Clutch",
-    "Pull%", "Cent%", "Oppo%", "GB%", "FB%", "LD%", "LA",
-    "FRV", "OAA", "ARM", "RANGE", "DRS", "TZ", "FRM", "UZR", "bWAR", "Age",
+        "Age",
     ],
 }
 
